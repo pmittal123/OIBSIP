f0e9db5e24dfa3350a163e53d670c7c328aaeac4
pmittal123 <priyanshu1mittal@gmail.com>
2025-08-26 23:42:31 +0530

    Delete LEVEL 2 directory

--- LEVEL 2/to-do-web-app/script.js
+++
@@ -1,8 +0,0 @@
-let text = document.getElementById("texts").value;
-let time = document.getElementById("times").value;
-let textarea = document.getElementById("disc").value;
-let button = document.getElementById("add");
-
-button.addEventListener('onclick' , ()=>{
-    
-});
